00aa9b6cb65805010e1ce88ca8c61c0e75243ec9
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-08 17:18:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
  final_results = data.each_with_object({}) do |(key, value), final_array|
    value.each do |inner_key, names|
      names.each do |name|
@@ -14,4 +15,11 @@ def nyc_pigeon_organizer(data)
   end      
 end
 end
+=======
+ hash.each_with_object({}) do |(key, value), final_array|
+   binding.pry
+   value.each do |inner_key, names|
+     final_array
+  end          
+>>>>>>> 869e4f5a67b64ef6e84e6d3e00d96a3c193e3e33
 end
